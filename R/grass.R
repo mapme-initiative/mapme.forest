c8ddb5ecf8410518064901a5633728464b630a61
Darius Görgen <darius2402@web.de>
2021-06-30 21:40:06 +0200

    added option to hide grass output messages

--- R/grass.R
+++ R/grass.R
@@ -27,6 +27,7 @@
 #'   will be written to. If files with the same name as specified by the values in
 #'   \code{idcol} are present, their calculation is skipped without warning.
 #' @param saveRaster Logical indicating if raster are to be saved to disk
+#' @param hideoutput Logical indicating if grass output should not be printed in console.
 #' @export statsGRASS
 #' @name statsGRASS
 #' @importFrom sf st_drop_geometry st_geometry st_crs st_transform st_write
@@ -46,11 +47,10 @@
 statsGRASS <- function(grass, addon_base,
                        areas, tree_cover, tree_loss, tree_co2,
                        idcol, thresholdClump, thresholdCover,
-                       years, outdir = NULL, saveRaster){
+                       years, outdir = NULL, saveRaster, hideoutput = FALSE){
 
   # get unique ids from idcol
   if (saveRaster) dir.create(outdir, showWarnings = F)
-
   ids = unlist(st_drop_geometry(areas[, idcol]))
   if (length(unique(ids)) != length(ids)){
     stop(paste0("Data in column ", idcol, " does not uniquley identify observations."))
@@ -74,11 +74,11 @@ statsGRASS <- function(grass, addon_base,
             location = "run",
             addon_base = addon_base,
             mapset = "PERMANENT", override = T)
-  execGRASS("g.proj", flags = "c", epsg = 4326)
+  execGRASS("g.proj", flags = "c", epsg = 4326, intern = hideoutput)
 
   if(!file.exists(file.path(addon_base, "bin", "r.area"))){
     dir.create(addon_base, showWarnings = F, recursive = T)
-    execGRASS("g.extension", extension = "r.area", prefix = addon_base)
+    execGRASS("g.extension", extension = "r.area", prefix = addon_base, intern = hideoutput)
   }
 
   for (i in 1:length(polies)){
@@ -94,7 +94,7 @@ statsGRASS <- function(grass, addon_base,
     }
     # write polygon to disk
     poly = st_transform(polies[[i]], proj_raster)
-    st_write(poly, file.path(rundir, paste0("poly_",i, ".gpkg")))
+    st_write(poly, file.path(rundir, paste0("poly_",i, ".gpkg")), quiet = TRUE)
 
     # create mapset in in region and update region to default
     initGRASS(gisBase = grass,
@@ -103,7 +103,7 @@ statsGRASS <- function(grass, addon_base,
               location = "run",
               addon_base = addon_base,
               mapset = i, override = T)
-    execGRASS("g.region", flags = "d")
+    execGRASS("g.region", flags = "d", intern = hideoutput)
 
 
 
@@ -111,56 +111,56 @@ statsGRASS <- function(grass, addon_base,
       ext = as.numeric(st_bbox(poly))
       # warp rasters to poly extent
       raw_command = paste0('gdalwarp -te ', paste(ext, collapse = " "), ' %s ', file.path(rundir, "%s"))
-      system(sprintf(raw_command, tree_cover, paste0(i, "_poly_cover.tif")))
-      system(sprintf(raw_command, tree_loss, paste0(i, "_poly_loss.tif")))
-      system(sprintf(raw_command, tree_co2, paste0(i, "_poly_co2.tif")))
+      system(sprintf(raw_command, tree_cover, paste0(i, "_poly_cover.tif")), intern = hideoutput)
+      system(sprintf(raw_command, tree_loss, paste0(i, "_poly_loss.tif")), intern = hideoutput)
+      system(sprintf(raw_command, tree_co2, paste0(i, "_poly_co2.tif")), intern = hideoutput)
 
       # read in rasters
-      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = file.path(rundir, paste0(i, "_poly_cover.tif")), output = "raw_cover")
-      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = file.path(rundir, paste0(i, "_poly_loss.tif")), output = "loss")
-      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = file.path(rundir, paste0(i, "_poly_co2.tif")), output = "co2")
+      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = file.path(rundir, paste0(i, "_poly_cover.tif")), output = "raw_cover", intern = hideoutput)
+      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = file.path(rundir, paste0(i, "_poly_loss.tif")), output = "loss", intern = hideoutput)
+      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = file.path(rundir, paste0(i, "_poly_co2.tif")), output = "co2", intern = hideoutput)
 
       # set location
-      execGRASS("g.region", raster= "raw_cover", flags = "p")
+      execGRASS("g.region", raster= "raw_cover", flags = "p", intern = hideoutput)
       # recode cover to binary
       writeLines(paste0("0 thru ",thresholdCover-1," = 0\n",thresholdCover," thru 100 = 1"), file.path(rundir, "rcl.txt"))
-      execGRASS("r.reclass", input = "raw_cover", output = "bc", rules =  file.path(rundir, "rcl.txt"))
-      execGRASS("r.mapcalc", expression = '"binary_cover = bc"')
+      execGRASS("r.reclass", input = "raw_cover", output = "bc", rules =  file.path(rundir, "rcl.txt"), intern = hideoutput)
+      execGRASS("r.mapcalc", expression = '"binary_cover = bc"', intern = hideoutput)
       # detect clumps
-      execGRASS("r.clump", input = "binary_cover", output = "clump")
+      execGRASS("r.clump", input = "binary_cover", output = "clump", intern = hideoutput)
       # remove smaller areas than threshold
-      execGRASS(file.path(addon_base, "bin", "r.area"), input = "clump", output = "clump_rm", lesser = thresholdClump, flags = "b")
+      execGRASS(file.path(addon_base, "bin", "r.area"), input = "clump", output = "clump_rm", lesser = thresholdClump, flags = "b", intern = hideoutput)
       # exclude removed pixels from cover raster
-      execGRASS("r.mapcalc", expression = '"y2000 = binary_cover * clump_rm"')
+      execGRASS("r.mapcalc", expression = '"y2000 = binary_cover * clump_rm"', intern = hideoutput)
       # system("r.out.gdal -cm --overwrite in=clean_cover out=clean_cover.tif")
       # create output group
-      execGRASS("i.group", group = "output", input = "raw_cover,loss,co2,y2000" )
+      execGRASS("i.group", group = "output", input = "raw_cover,loss,co2,y2000", intern = hideoutput)
       # loop trhough values to get yearly layers
       vals = as.numeric(str_sub(years, -2, -1))
       for (j in 1:length(vals)){
         writeLines(paste0("0 = 1\n1 thru ", vals[j], " = 0\n", vals[j]+1, " thru 100 = 1"), file.path(rundir, "rcl.txt"))
         # recode loss as all values equal and below val to 0 and all values above val to 1, call result layer loss_t
-        execGRASS("r.reclass", input = "loss", output ="lt", rules = file.path(rundir, "rcl.txt"), flags = "overwrite")
-        execGRASS("r.mapcalc", expression = '"loss_t = lt"', flags = "overwrite")
+        execGRASS("r.reclass", input = "loss", output ="lt", rules = file.path(rundir, "rcl.txt"), flags = "overwrite", intern = hideoutput)
+        execGRASS("r.mapcalc", expression = '"loss_t = lt"', flags = "overwrite", intern = hideoutput)
         # calculate cover_t based on base cover times loss_t
-        execGRASS("r.mapcalc", expression = paste0('"y',years[j],' = y2000 * loss_t"'))
-        execGRASS("i.group", group = "output", input = paste0("y",years[j]))
+        execGRASS("r.mapcalc", expression = paste0('"y',years[j],' = y2000 * loss_t"'), intern = hideoutput)
+        execGRASS("i.group", group = "output", input = paste0("y",years[j]), intern = hideoutput)
       }
 
     } else { # in case file is alread present
 
-      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = outname, output = "infile")
-      execGRASS("g.region", raster= "infile.1", flags = "p")
-      execGRASS("g.rename", raster=c("infile.1,raw_cover,infile.2,loss,infile.3,co2,infile.4,y2000"))
+      execGRASS("r.in.gdal", flags = c("o", "overwrite"), input = outname, output = "infile", intern = hideoutput)
+      execGRASS("g.region", raster= "infile.1", flags = "p", intern = hideoutput)
+      execGRASS("g.rename", raster=c("infile.1,raw_cover,infile.2,loss,infile.3,co2,infile.4,y2000"), intern = hideoutput)
 
-      execGRASS("i.group", group = "output", input = "raw_cover,loss,co2,y2000" )
+      execGRASS("i.group", group = "output", input = "raw_cover,loss,co2,y2000", intern = hideoutput)
 
       infiles = paste("infile", 5:(4+length(years)), sep = ".")
       outfiles = paste("y", years, sep ="")
 
       # rename the yearly layers
       for (j in 1:length(infiles)){
-        execGRASS("g.rename", raster=paste0(infiles[j],",",outfiles[j]))
+        execGRASS("g.rename", raster=paste0(infiles[j],",",outfiles[j]), intern = hideoutput)
       }
 
       # system("g.list type=raster pattern=*")
@@ -168,14 +168,14 @@ statsGRASS <- function(grass, addon_base,
 
     # --------------------------------- ZONAL STATS ---------------------------#
     # calculate zonal stats
-    execGRASS("v.in.ogr", input = file.path(rundir, paste0("poly_", i, ".gpkg")), output = "poly" )
-    execGRASS("v.to.rast", input = "poly", type = "area", output = "polyr", use = "val", value = 1)
-    execGRASS("r.null", map = "polyr", null = 0)
+    execGRASS("v.in.ogr", input = file.path(rundir, paste0("poly_", i, ".gpkg")), output = "poly", intern = hideoutput)
+    execGRASS("v.to.rast", input = "poly", type = "area", output = "polyr", use = "val", value = 1, intern = hideoutput)
+    execGRASS("r.null", map = "polyr", null = 0, intern = hideoutput)
     # calculate yearly forest cover statistics
     layernames = paste("y", c(2000,years), sep="")
 
     area_estimates = lapply(1:length(layernames), function(r){
-      execGRASS("r.mapcalc", expression = paste0('"area_t = ',layernames[r],' * polyr"'), flags = "overwrite")
+      execGRASS("r.mapcalc", expression = paste0('"area_t = ',layernames[r],' * polyr"'), flags = "overwrite", intern = hideoutput)
       # execGRASS("r.univar", map = "area_t")
       estimate = execGRASS("r.stats", input = "area_t", flags = c("n", "a"), intern = TRUE)
 
@@ -214,11 +214,11 @@ statsGRASS <- function(grass, addon_base,
       for(j in index){
         writeLines(paste0("0 = 0\n1 = 1\n2 = 0"), file.path(rundir, "rcl.txt"))
 
-        execGRASS("r.mapcalc", expression = paste0('"mask = ',layernames[j-1],' + ',layernames[j],'"'), flags = "overwrite")
-        execGRASS("r.reclass", input = "mask", output = "mask2", rules =  file.path(rundir, "rcl.txt"), flags = "overwrite")
-        execGRASS("r.mapcalc", expression = '"mask2 = mask2"', flags = "overwrite")
-        execGRASS("r.mapcalc", expression = '"mask2 = mask2 * polyr"', flags = "overwrite")
-        execGRASS("r.mapcalc", expression = '"mask2 = mask2 * co2"', flags = "overwrite")
+        execGRASS("r.mapcalc", expression = paste0('"mask = ',layernames[j-1],' + ',layernames[j],'"'), flags = "overwrite", intern = hideoutput)
+        execGRASS("r.reclass", input = "mask", output = "mask2", rules =  file.path(rundir, "rcl.txt"), flags = "overwrite", intern = hideoutput)
+        execGRASS("r.mapcalc", expression = '"mask2 = mask2"', flags = "overwrite", intern = hideoutput)
+        execGRASS("r.mapcalc", expression = '"mask2 = mask2 * polyr"', flags = "overwrite", intern = hideoutput)
+        execGRASS("r.mapcalc", expression = '"mask2 = mask2 * co2"', flags = "overwrite", intern = hideoutput)
         # system("r.out.gdal input=mask2 output=mask2.tif --overwrite")
         output = execGRASS("r.univar", map = "mask2", intern = T)
 
@@ -256,11 +256,11 @@ statsGRASS <- function(grass, addon_base,
       if(!file.exists(outname)){
         # write resulting raster brick to disk
         # system(paste0('r.out.gdal -mc createopt="COMPRESS=LZW" input=output output=',outname,' format=GTiff'))
-        execGRASS("r.out.gdal", createopt = '"COMPRESS=LZW"', input = "output", output = outname, format = "GTiff", flags = c("m", "c"))
+        execGRASS("r.out.gdal", createopt = '"COMPRESS=LZW"', input = "output", output = outname, format = "GTiff", flags = c("m", "c"), intern = hideoutput)
       }
     }
     # delete all files in current grass mapset
-    execGRASS("g.remove", type = "all", pattern = "*", flags = "f")
+    execGRASS("g.remove", type = "all", pattern = "*", flags = "f", intern = hideoutput, ignore.stderr = TRUE)
     # remove file from grass db
     unlink(file.path(rundir, "gisdb", "run", i), recursive = T)
     # remove files from parent dir
